7ae3bb34cb0b2582d626ed1fd986f1c10a494191
Sissi Zhang <156721167+SISSI-Y-ZHANG@users.noreply.github.com>
2025-07-14 14:44:50 -0700

    Deleted code for object mask generation

--- generate_data.py
+++ generate_data.py
@@ -18,11 +18,6 @@ SCALE_EPS = 0.2 # Size deviation for randomness
 
 ZOOM_DISTANCE = 10 # Distance to zoom the camera backward from an object
 
-MASK_PASS_IDX = 1 # Pass index for objects we want to generate masks on
-DEFAULT_PASS_IDX = 0 # Default pass index for all other objects
-
-GET_MASK = False  # Set to True if you want to generate mask for each object the camera focuses on
-
 
 
 # Define output locations
@@ -192,66 +187,6 @@ def rotate_object(obj):
 
 
 
-# === RENDER MASK ===
-
-def set_compositor_for_masks(scene):
-    # Enable compositing with nodes
-    scene.use_nodes = True
-    tree = scene.node_tree
-    tree.nodes.clear()
-
-    # Add necessary nodes
-    render_layers = tree.nodes.new(type='CompositorNodeRLayers')    # Render layers
-    composite = tree.nodes.new(type='CompositorNodeComposite')      # Composite
-    id_mask = tree.nodes.new(type='CompositorNodeIDMask')           # ID Mask
-    viewer = tree.nodes.new(type='CompositorNodeViewer')            # Viewer
-
-    # Set Pass Index to match the object
-    id_mask.index = MASK_PASS_IDX
-    
-    # Create Links between nodes
-    tree.links.new(render_layers.outputs['IndexOB'], id_mask.inputs['ID value'])
-    tree.links.new(id_mask.outputs['Alpha'], viewer.inputs['Image'])
-    tree.links.new(id_mask.outputs['Alpha'], composite.inputs['Image'])
-
-
-
-# === ADD BACKGROUND ===
-
-def add_background(scene, bg_image_path):
-    # Enable compositing with nodes
-    scene.use_nodes = True
-    tree = scene.node_tree
-    tree.nodes.clear()
-
-    nodes = tree.nodes
-    links = tree.links
-
-    # Create nodes
-    render_layers = nodes.new(type='CompositorNodeRLayers')
-    composite_node = nodes.new(type='CompositorNodeComposite')
-    bg_image_node = nodes.new(type='CompositorNodeImage')
-    alpha_over = nodes.new(type='CompositorNodeAlphaOver')
-
-    # Set background scale to match the render size
-    scale_node = nodes.new(type='CompositorNodeScale')
-    scale_node.space = 'RENDER_SIZE'
-
-    # Load your image
-    bg_image = bpy.data.images.load(bg_image_path)
-    bg_image_node.image = bg_image
-
-    # Link nodes
-    links.new(bg_image_node.outputs['Image'], scale_node.inputs['Image'])
-    links.new(scale_node.outputs['Image'], alpha_over.inputs[1])       # Background
-    links.new(render_layers.outputs['Image'], alpha_over.inputs[2])    # Foreground
-    links.new(alpha_over.outputs['Image'], composite_node.inputs['Image'])
-
-    # Set render settings for transparency
-    bpy.context.scene.render.film_transparent = True
-
-
-
 def add_hdri_background(scene):
     world = scene.world
     world.use_nodes = True
@@ -296,28 +231,6 @@ def add_hdri_background(scene):
 
 
 # === RENDER OBJECTS ===
-
-def get_object_mask(obj, scene, output_folder, num_of_view):
-    # Enable compositor tree
-    scene.use_nodes = True
-    
-    # Setup the object pass index
-    obj.pass_index = MASK_PASS_IDX
-
-    # Set output path and file format
-    mask_path = rf"{output_folder}\images\{obj.name}_view_{num_of_view}.png"
-    scene.render.filepath = mask_path
-    scene.render.image_settings.file_format = 'PNG'
-    scene.render.image_settings.color_mode = 'BW'  # Black & White mask
-
-    # Render and save the result
-    bpy.ops.render.render(write_still=True)
-
-    # Reset index
-    obj.pass_index = DEFAULT_PASS_IDX
-
-    # Disable compositor tree
-    scene.use_nodes = False
 
 def traverse_tree(t):
     yield t
@@ -425,10 +338,6 @@ def capture_views(obj, camera, scene, output_folder, zoom_distance, get_mask):
             for bbox, label_idx in visible_bboxes.items():
                 x_center, y_center, width, height = bbox
                 f.write(f"{label_idx} {x_center:.6f} {y_center:.6f} {width:.6f} {height:.6f}\n")
-        
-        # Save the mask
-        if get_mask:
-            get_object_mask(obj, scene, output_folder, i+1)
 
 
 
@@ -490,17 +399,7 @@ def render_loop(collection_name, output_location, bg_image_path, zoom_distance, 
 
 if __name__ == "__main__":
     # Renderer setup
-    if GET_MASK:
-        scene.render.engine = 'CYCLES'
-        scene.cycles.device = 'GPU'
-
-        # Enable object index pass
-        bpy.context.view_layer.use_pass_object_index = True
-
-        # Build compositor tree for masks
-        set_compositor_for_masks()
-    else:
-        scene.render.engine = 'BLENDER_EEVEE_NEXT'
+    scene.render.engine = 'BLENDER_EEVEE_NEXT'
     
     add_hdri_background(scene)
 
