a1137cc53ae7b428ebf108c778bab9a0ff8e3872
Sissi Zhang <156721167+SISSI-Y-ZHANG@users.noreply.github.com>
2025-07-30 11:40:46 -0700

    Added changes to objects importing, restructured files

--- generate_data.py
+++ generate_data.py
@@ -545,22 +545,127 @@ def get_selected_objects(original_transforms, label_names, num_obj, num_distract
 
 
 
+# === CLEAR STAGE ===
+
+def traverse_tree(t):
+    yield t
+    for child in t.children:
+        yield from traverse_tree(child)
+
+def clear_stage(scene):
+    # Clear all collections
+    for coll in traverse_tree(scene.collection):
+        for obj in coll.objects:
+            # Unlink from all collections to avoid dangling links
+            for coll in obj.users_collection:
+                coll.objects.unlink(obj)
+                
+            # Remove the object from the scene
+            bpy.data.objects.remove(obj, do_unlink=True)
+        
+        # Keep the default Scene Collection
+        if coll.name != "Scene Collection":
+            scene.collection.children.unlink(coll)
+
+def add_default_obj(scene):
+    # Add new camera
+    camera_data = bpy.data.cameras.new(name="Camera")
+    camera_object = bpy.data.objects.new("Camera", camera_data)
+
+    # Set camera location and rotation
+    camera_object.location = (0, -5, 3)
+    camera_object.rotation_euler = (1.1, 0, 0)
+
+    # Link camera to scene
+    bpy.context.collection.objects.link(camera_object)
+    bpy.context.scene.camera = camera_object
+    
+    
+    # Add new sunlight
+    light_data = bpy.data.lights.new(name="Sun", type='SUN')
+    light_object = bpy.data.objects.new(name="Sun", object_data=light_data)
+
+    # Set light location and rotation
+    light_object.location = (5, -5, 10)
+    light_object.rotation_euler = (0.7854, 0, 0.7854)  # 45 degrees down and to side
+
+    # Link light to scene
+    bpy.context.collection.objects.link(light_object)
+    
+    #bpy.ops.mesh.primitive_cube_add(size=0.2, location=(0, 0, 0))
+
+
+
+# === IMPORT OBJECTS ===
+
+def import_obj(scene, obj_path):
+    
+    label_names = []
+
+    # Iterate through all category folders
+    category_folders = glob.glob(f"{obj_path}/*/")
+    for category_folder in category_folders:
+        category_name = os.path.basename(os.path.dirname(category_folder))
+        
+        # Create new collection
+        new_coll = bpy.data.collections.new(category_name)
+        scene.collection.children.link(new_coll)
+
+        if category_name != "distractors" and category_name not in label_names:
+            label_names.append(category_name)
+
+        # Iterate through all object folders in the category
+        obj_folders = glob.glob(f"{category_folder}/*/")
+        for obj_folder in obj_folders:
+            obj_name = os.path.basename(os.path.dirname(obj_folder))
+            
+            # Make path for current object
+            file_path = os.path.join(obj_folder, f"{obj_name}.obj")
+            
+            # Import current object
+            bpy.ops.wm.obj_import(filepath=file_path)
+            
+            # Get the imported object
+            new_obj = bpy.context.view_layer.objects.active
+            
+            if not new_obj:
+                continue
+            
+            for coll in new_obj.users_collection:
+                coll.objects.unlink(new_obj)
+            
+            # Link the object to the new collection
+            new_coll.objects.link(new_obj)
+
+    return label_names  # Return the list of label names for further processing
+
+
+
 # === MAIN FUNCTION ===
 
 def main(args):
+    print("\n======================================== Main is running ========================================\n")
+    
+    clear_stage(scene)
+    print("Cleared the stage.")
+
+    add_default_obj(scene)
+    print("Added default objects (camera, light).")
+
     # Common object setups
     scene = bpy.context.scene
     camera = scene.camera
-    light = scene.objects["Light"]
+    light = scene.objects["Sun"]
     depsgraph = bpy.context.evaluated_depsgraph_get()
 
     # Renderer setup
     scene.render.engine = 'BLENDER_EEVEE_NEXT'
     scene.render.resolution_percentage = args.render_percentage * 100
-    
-    args.obj_path
-    label_names = ["screwdriver", "wrench"] # TODO: make it automate as well
 
+    
+    label_names = import_obj(scene, args.obj_path)
+    print(f"Imported objects: {label_names}")
+    
     # Give each object a unique pass index and record their coresponding labels
     pass_index_to_label = {}
     if args.use_ray_cast:
@@ -574,6 +679,7 @@ def main(args):
 
     # Set the output folder
     output_folder = setup_output_folder(args.output_path)
+    print(f"Set up output folder: {output_folder}")
     
     # Stores initial object transforms
     original_transforms = {} 
